251e552d09712e44da5026ee93ddfb74f56c5fda
AlexanderDergachev <31667079+AlexanderDergachev@users.noreply.github.com>
2020-05-12 19:02:47 +0300

    Updates

---
+++ precache-manifest.c58085a8a1ee669d3f3191d9497bd9b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e352b218c1344cdf6562faffe945db",
+    "url": "/rsoi-project/index.html"
+  },
+  {
+    "revision": "c327a7107531d0fbad96",
+    "url": "/rsoi-project/static/css/main.4d5f9674.chunk.css"
+  },
+  {
+    "revision": "820e46f29885c5ea2fda",
+    "url": "/rsoi-project/static/js/2.d327b532.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rsoi-project/static/js/2.d327b532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c327a7107531d0fbad96",
+    "url": "/rsoi-project/static/js/main.11195c16.chunk.js"
+  },
+  {
+    "revision": "d9ba6a741c1e6e0ef95f",
+    "url": "/rsoi-project/static/js/runtime-main.7e49d097.js"
+  }
+]);
